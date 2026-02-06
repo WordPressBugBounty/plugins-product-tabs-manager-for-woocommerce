c48d30de98b1814b347e52f80f0359c909bc7920
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 04:57:03 +0000

    1.2.1.2

--- product-tabs-manager-for-woocommerce/woocommerce-tab-manager.php
+++ product-tabs-manager-for-woocommerce/woocommerce-tab-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: Product Tabs Manager for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/product-tabs-manager-for-woocommerce/?utm_source=free_plugin&utm_medium=plugins&utm_campaign=tab_manager
  * Description: Manage product tabs. Set specific tabs per category or product
- * Version: 1.2.1.1
+ * Version: 1.2.1.2
  * Author: BeRocket
  * Requires at least: 5.0
  * Tested up to: 6.8
@@ -12,8 +12,8 @@
  * Author URI: https://berocket.com?utm_source=free_plugin&utm_medium=plugins&utm_campaign=tab_manager
  * Text Domain: product-tabs-manager-for-woocommerce
  * Domain Path: /languages/
- * WC tested up to: 10.0
+ * WC tested up to: 10.3
  */
-define( "BeRocket_tab_manager_version", '1.2.1.1' );
+define( "BeRocket_tab_manager_version", '1.2.1.2' );
 define( "BeRocket_tab_manager_file", __FILE__ );
 include_once('main.php');
